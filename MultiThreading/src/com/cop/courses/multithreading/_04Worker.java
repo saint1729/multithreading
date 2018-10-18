2e57eead4f5408575e82b28adb60fe26092d616e
Sai Nikhil <tsnlegend@gmail.com>
2018-10-18 23:31:01 +0530

    Completed watching and implementation till first 6 videos

--- MultiThreading/src/com/cop/courses/multithreading/_04Worker.java
+++ MultiThreading/src/com/cop/courses/multithreading/_04Worker.java
@@ -8,23 +8,80 @@ public class _04Worker {
 	
 	private Random random = new Random();
 	
+	private Object object1 = new Object();
+	private Object object2 = new Object();
+	
 	private List<Integer> list1 = new ArrayList<>();
 	private List<Integer> list2 = new ArrayList<>();
 	
 	
 	public void stageOne() {
-		try {
-			Thread.sleep(1);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		synchronized(object1) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			list1.add(random.nextInt(100));
 		}
-		
-		list1.add(random.nextInt(100));
+	}
+	
+	
+	public void stageTwo() {
+		synchronized(object2) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			list2.add(random.nextInt(100));
+		}
+	}
+	
+	
+	public void process() {
+		for(int i = 0; i < 1000; i++) {
+			stageOne();
+			stageTwo();
+		}
 	}
 	
 	
 	public void main() {
-		System.out.println("Hello");
+		System.out.println("Starting...");
+		
+		long start = System.currentTimeMillis();
+		
+		Thread t1 = new Thread(new Runnable() {
+			public void run() {
+				process();
+			}
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+			public void run() {
+				process();
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		long end = System.currentTimeMillis();
+		
+		System.out.println("Time taken = " + (end-start));
+		System.out.println("List1: " + list1.size() + " List2: " + list2.size());
+		
 	}
 	
 }
